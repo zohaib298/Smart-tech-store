6ce56a9e73d0ecd86dce35a36cb1072edf667c9f
Zohaib Mustafa <zohaibmustafa200@gmail.com>
2026-07-30 14:15:02 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,7 +90,39 @@
         </div>
     </div>
 </div>
-        <x-brands /> 
+        <div class="w-[95%] sm:w-[90%] lg:w-[80%] mx-auto my-16 text-center">
+
+            <h3 class="text-2xl font-semibold mb-8">TRUSTED BY SELLING BRANDS</h3>
+
+            <div class="grid grid-cols-2 sm:grid-cols-4 gap-6 items-center justify-items-center">
+
+                <div class="bg-white shadow rounded-xl p-4 w-full h-[90px] flex items-center justify-center">
+                    <img src="{{ asset('images/brands/dell-logo.png') }}"
+                         alt="Dell"
+                         class="max-h-[50px] object-contain">
+                </div>
+
+                <div class="bg-white shadow rounded-xl p-4 w-full h-[90px] flex items-center justify-center">
+                    <img src="{{ asset('images/brands/hp-logo.png') }}"
+                         alt="HP"
+                         class="max-h-[50px] object-contain">
+                </div>
+
+                <div class="bg-white shadow rounded-xl p-4 w-full h-[90px] flex items-center justify-center">
+                    <img src="{{ asset('images/brands/lenovo-logo.png') }}"
+                         alt="Lenovo"
+                         class="max-h-[50px] object-contain">
+                </div>
+
+                <div class="bg-white shadow rounded-xl p-4 w-full h-[90px] flex items-center justify-center">
+                    <img src="{{ asset('images/brands/asus-logo.png') }}"
+                         alt="Asus"
+                         class="max-h-[50px] object-contain">
+                </div>
+
+            </div>
+
+        </div>
         <div class="flex flex-col md:flex-row gap-4 w-[95%] sm:w-[90%] lg:w-[80%] mx-auto my-10">
 
     <div class="bg-gray-100 w-full p-5 rounded">
