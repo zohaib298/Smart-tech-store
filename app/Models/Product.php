3dfb3b00153ba1965ad794cfc506c22dc5e1af25
Zohaib Mustafa <zohaibmustafa200@gmail.com>
2026-07-30 11:44:08 +0500

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
-class product extends Model
+class Product extends Model
 {
     protected $table = 'products'; 
    public function seller()
